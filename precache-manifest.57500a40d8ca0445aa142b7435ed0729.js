8bea5bb0a59ba36065df251dd02dd29e7b8bdd90
Ammyy9908 <sb78639@gmail.com>
2020-03-29 08:43:52 +0530

    Updates

---
+++ precache-manifest.57500a40d8ca0445aa142b7435ed0729.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef42947617b3902fdc5a6954e114f56",
+    "url": "/quotable/index.html"
+  },
+  {
+    "revision": "049a00e543d0d79c84bd",
+    "url": "/quotable/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "64dc03124ff91286829b",
+    "url": "/quotable/static/js/2.fb5e1143.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotable/static/js/2.fb5e1143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "049a00e543d0d79c84bd",
+    "url": "/quotable/static/js/main.cf4791fc.chunk.js"
+  },
+  {
+    "revision": "0e2cb424736c4269b02f",
+    "url": "/quotable/static/js/runtime-main.88f8ebcd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quotable/static/media/logo.5d5d9eef.svg"
+  }
+]);
